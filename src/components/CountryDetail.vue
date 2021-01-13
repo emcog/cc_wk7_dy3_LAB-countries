6991f2993da20b9df0356be4311cd1a6c84ac518
emcog <robin.leonard@gmail.com>
2021-01-13 17:17:38 +0000

    updates

--- src/components/CountryDetail.vue
+++ src/components/CountryDetail.vue
@@ -1,10 +1,16 @@
-<template>
-  <div></div>
+<template lang="html">
+  <div v-if="country">
+    <span>Bananas</span>
+    <h2>{{ country.name }}</h2>
+    <span>{{ country.capital }}</span>
+    <span>{{ country.population }}</span>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: "country-detail",
+  props:['country'] //?????????????????
 }
 </script>
 
